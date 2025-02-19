ee2edb2f57febf6903b2d09e1ab2c08798dbd35a
Md. Zubaer Ahammed <zubaerahammed223@gmail.com>
2025-02-19 12:12:43 -0500

    Implement promises for task 1-4 (get books, books by isbn, title, author)

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,7 +26,8 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-
+  //res.send(JSON.stringify(books,null,4));
+  
   let getBooksPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve(books);
@@ -44,24 +45,72 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-  res.send(books[isbn]);
+  //const isbn = req.params.isbn;
+  //res.send(books[isbn]);
+
+  let getBooksPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const isbn = req.params.isbn;
+        resolve(books[isbn]);
+    }, 3000);
+  });
+
+  getBooksPromise.then((book) => {
+    res.json(book);
+  })
+  .catch((error) => {
+    res.status(404).json({ message: error.message });
+  });
+
+
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  const author = req.params.author;
-  let filtered_books = Object.values(books).filter((book) => book.author === author);
+  //const author = req.params.author;
+  //let filtered_books = Object.values(books).filter((book) => book.author === author);
   // Send the filtered_books array as the response to the client
-  res.send(filtered_books);
+  //res.send(filtered_books);  
+  
+  let getBooksPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const author = req.params.author;
+        let filtered_books = Object.values(books).filter((book) => book.author === author);      
+        resolve(filtered_books);
+    }, 3000);
+  });
+
+  getBooksPromise.then((books) => {
+    res.json(books);
+  })
+  .catch((error) => {
+    res.status(404).json({ message: error.message });
+  });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  const title = req.params.title;
-  let filtered_books = Object.values(books).filter((book) => book.title === title);
+  //const title = req.params.title;
+  //let filtered_books = Object.values(books).filter((book) => book.title === title);
   // Send the filtered_books array as the response to the client
-  res.send(filtered_books);
+  //res.send(filtered_books);
+
+  let getBooksPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const title = req.params.title;
+        let filtered_books = Object.values(books).filter((book) => book.title === title);     
+        resolve(filtered_books);
+    }, 3000);
+  });
+
+  getBooksPromise.then((book) => {
+    res.json(book);
+  })
+  .catch((error) => {
+    res.status(404).json({ message: error.message });
+  });
+
+
 });
 
 //  Get book review

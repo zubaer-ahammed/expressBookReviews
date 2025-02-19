88bc1d6c21a5424cc0148bd091f105e5fe976ec1
Md. Zubaer Ahammed <zubaerahammed223@gmail.com>
2025-02-19 08:57:25 -0500

    getting the book details based on the author  and title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,14 +25,20 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let filtered_books = Object.values(books).filter((book) => book.author === author);
+  // Send the filtered_books array as the response to the client
+  res.send(filtered_books);
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  let filtered_books = Object.values(books).filter((book) => book.title === title);
+  // Send the filtered_books array as the response to the client
+  res.send(filtered_books);
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 //  Get book review
